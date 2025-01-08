0a56f3ece4383522dfcda00fd5a7e6f5b7afe09d
vbusa30 <varshabusa30@gmail.com>
2025-01-08 23:14:40 +0530

    ec2 instance

--- my-ec2/bin/my-ec2.ts
+++ my-ec2/bin/my-ec2.ts
@@ -15,7 +15,7 @@ new MyEc2Stack(app, 'MyEc2Stack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '992382386705', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- my-ec2/lib/my-ec2-stack.ts
+++ my-ec2/lib/my-ec2-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,17 @@ export class MyEc2Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // define default vpc to use from existing aws region
+    const vpc = ec2.Vpc.fromLookup(this, 'ExistingVPC' ,{
+      isDefault: true
+    });
+    
+    // creating ec2 instance
+    const varshainstance = new ec2.Instance(this, 'varshamachine' ,{
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: new ec2.AmazonLinuxImage(),
+      keyName: 'splunk-key',
+    })
   }
 }
